4080525e9bb23a7f60037e861a3f55c3fbc0d207
Richard Reimer <rreimer@mail.uni-mannheim.de>
2015-10-09 13:27:08 +0200

    update for ManagementALlRandom with videoList information

--- src/main/java/no/uio/ifi/management/ManagementAllRandom.java
+++ src/main/java/no/uio/ifi/management/ManagementAllRandom.java
@@ -6,7 +6,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
@@ -14,6 +16,7 @@ import java.util.Random;
 import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Thumbnail;
+import com.google.api.services.youtube.model.Video;
 
 import no.uio.ifi.guis.YTDashGUI;
 import no.uio.ifi.models.Search;
@@ -31,7 +34,7 @@ public class ManagementAllRandom {
 	}
 
 	/*
-	 * write random videolinks to disk change directory!
+	 * write random videolinks to disk change directory!!
 	 */
 	public ManagementAllRandom(String maxPower) {
 		try {
@@ -76,60 +79,60 @@ public class ManagementAllRandom {
 		};
 
 		one.start();
-		//
-		// Thread two = new Thread() {
-		// public void run() {
-		// try {
-		// for (int i = 0; i < 10000000; i++) {
-		// String rnd = mng.randomUrlGenerator();
-		// mng.searchBaseOnRandomID("watch?v=" + rnd,2);
-		// }
-		// Thread.sleep(1);
-		//
-		// System.out.println("thread error.");
-		// } catch(InterruptedException v) {
-		// System.out.println(v);
-		// }
-		// }
-		// };
-		//
-		// two.start();
-		//
-		// Thread three = new Thread() {
-		// public void run() {
-		// try {
-		// for (int i = 0; i < 10000000; i++) {
-		// String rnd = mng.randomUrlGenerator();
-		// mng.searchBaseOnRandomID("watch?v=" + rnd,3);
-		// }
-		// Thread.sleep(1);
-		//
-		// System.out.println("thread error.");
-		// } catch(InterruptedException v) {
-		// System.out.println(v);
-		// }
-		// }
-		// };
-		//
-		// three.start();
-		//
-		// Thread four = new Thread() {
-		// public void run() {
-		// try {
-		// for (int i = 0; i < 10000000; i++) {
-		// String rnd = mng.randomUrlGenerator();
-		// mng.searchBaseOnRandomID("watch?v=" + rnd,4);
-		// }
-		// Thread.sleep(1);
-		//
-		// System.out.println("thread error.");
-		// } catch(InterruptedException v) {
-		// System.out.println(v);
-		// }
-		// }
-		// };
-		//
-		// four.start();
+
+		Thread two = new Thread() {
+			public void run() {
+				try {
+					for (int i = 0; i < 10000000; i++) {
+						String rnd = mng.randomUrlGenerator();
+						mng.searchBaseOnRandomID("watch?v=" + rnd, 2);
+					}
+					Thread.sleep(1);
+
+					System.out.println("thread error.");
+				} catch (InterruptedException v) {
+					System.out.println(v);
+				}
+			}
+		};
+
+		two.start();
+
+		Thread three = new Thread() {
+			public void run() {
+				try {
+					for (int i = 0; i < 10000000; i++) {
+						String rnd = mng.randomUrlGenerator();
+						mng.searchBaseOnRandomID("watch?v=" + rnd, 3);
+					}
+					Thread.sleep(1);
+
+					System.out.println("thread error.");
+				} catch (InterruptedException v) {
+					System.out.println(v);
+				}
+			}
+		};
+
+		three.start();
+
+		Thread four = new Thread() {
+			public void run() {
+				try {
+					for (int i = 0; i < 10000000; i++) {
+						String rnd = mng.randomUrlGenerator();
+						mng.searchBaseOnRandomID("watch?v=" + rnd, 4);
+					}
+					Thread.sleep(1);
+
+					System.out.println("thread error.");
+				} catch (InterruptedException v) {
+					System.out.println(v);
+				}
+			}
+		};
+
+		four.start();
 
 	}
 
@@ -173,31 +176,68 @@ public class ManagementAllRandom {
 			List<SearchResult> searchResults = search.getVideoLinkFromKeyWord(keyword);
 			ListIterator<SearchResult> iteratorSearchResults = searchResults.listIterator();
 			System.out.println("\n=============================================================");
-			System.out.println(
-					"   First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + keyword + "\".");
+			System.out
+					.println("   First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on " + keyword + ".");
 			System.out.println("=============================================================\n");
 
 			if (!iteratorSearchResults.hasNext()) {
 				System.out.println(" There aren't any results for your query.");
 			}
 
+			String duration = null;
+			BigInteger likes = null;
+			BigInteger favourites = null;
+			BigInteger views = null;
+			BigInteger dislikes = null;
+			BigInteger comments = null;
+			Video singleVideoList = null;
+
 			while (iteratorSearchResults.hasNext()) {
 
-				SearchResult singleVideo = iteratorSearchResults.next();
-				ResourceId rId = singleVideo.getId();
+				SearchResult singleVideoSearchList = iteratorSearchResults.next();
+				ResourceId rId = singleVideoSearchList.getId();
+
+				List<Video> videoList = search.getVideoList(rId.getVideoId());
+				Iterator<Video> iteratorVideoResults = videoList.iterator();
+				if (!iteratorVideoResults.hasNext()) {
+					System.out.println(" There aren't any results for your query.");
+				}
+
+				while (iteratorVideoResults.hasNext()) {
+
+					singleVideoList = iteratorVideoResults.next();
+
+					duration = singleVideoList.getContentDetails().getDuration();
+					views = singleVideoList.getStatistics().getViewCount();
+					likes = singleVideoList.getStatistics().getLikeCount();
+					favourites = singleVideoList.getStatistics().getFavoriteCount();
+					dislikes = singleVideoList.getStatistics().getDislikeCount();
+					comments = singleVideoList.getStatistics().getCommentCount();
+
+				}
 
 				// Confirm that the result represents a video. Otherwise, the
 				// item will not contain a video ID.
 				if (rId.getKind().equals("youtube#video")) {
-					Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
-					System.out.println(singleVideo);
-					System.out.println("https://www.youtube.com/watch?v=" + rId.getVideoId());
-					System.out.println(singleVideo.getSnippet().getTitle());
+					// Thumbnail thumbnail =
+					// singleVideoSearch.getSnippet().getThumbnails().getDefault();
+
+					System.out.println(singleVideoSearchList);
+					System.out.println(singleVideoList);
+					// System.out.println("https://www.youtube.com/watch?v=" +
+					// rId.getVideoId());
+					System.out.println(singleVideoSearchList.getSnippet().getTitle());
+					System.out.println("Duration of the video:" + duration);
+					System.out.println("Views:" + views);
+					System.out.println("Likes:" + likes);
+					System.out.println("Disklikes:" + dislikes);
+					System.out.println("Favourites:" + favourites);
+					System.out.println("Comments:" + comments);
 
 					try {
 
 						writer.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
-						writer.write(singleVideo.getSnippet().getTitle());
+						writer.write(singleVideoSearchList.getSnippet().getTitle());
 						writer.write(System.lineSeparator());
 						counter++;
 						System.out.println(counter);
@@ -210,134 +250,146 @@ public class ManagementAllRandom {
 			}
 		}
 
-		else if (threadNumber == 2) {
-			List<SearchResult> searchResults = search.getVideoLinkFromKeyWord(keyword);
-			ListIterator<SearchResult> iteratorSearchResults = searchResults.listIterator();
-			System.out.println("\n=============================================================");
-			System.out.println(
-					"   First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + keyword + "\".");
-			System.out.println("=============================================================\n");
-
-			if (!iteratorSearchResults.hasNext()) {
-				System.out.println(" There aren't any results for your query.");
-			}
-
-			while (iteratorSearchResults.hasNext()) {
-
-				SearchResult singleVideo = iteratorSearchResults.next();
-				ResourceId rId = singleVideo.getId();
-
-				// Confirm that the result represents a video. Otherwise, the
-				// item will not contain a video ID.
-				if (rId.getKind().equals("youtube#video")) {
-					// Thumbnail thumbnail =
-					// singleVideo.getSnippet().getThumbnails().getDefault();
-					// System.out.println(singleVideo);
-					System.out.println("https://www.youtube.com/watch?v=" + rId.getVideoId());
-					System.out.println(singleVideo.getSnippet().getTitle());
-
-					try {
-
-						writer2.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
-						writer2.write(singleVideo.getSnippet().getTitle());
-						writer2.write(System.lineSeparator());
-						counter++;
-						System.out.println(counter);
-
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			}
-		} else if (threadNumber == 3) {
-			List<SearchResult> searchResults = search.getVideoLinkFromKeyWord(keyword);
-			ListIterator<SearchResult> iteratorSearchResults = searchResults.listIterator();
-			System.out.println("\n=============================================================");
-			System.out.println(
-					"   First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + keyword + "\".");
-			System.out.println("=============================================================\n");
-
-			if (!iteratorSearchResults.hasNext()) {
-				System.out.println(" There aren't any results for your query.");
-			}
-
-			while (iteratorSearchResults.hasNext()) {
-
-				SearchResult singleVideo = iteratorSearchResults.next();
-				ResourceId rId = singleVideo.getId();
-
-				// Confirm that the result represents a video. Otherwise, the
-				// item will not contain a video ID.
-				if (rId.getKind().equals("youtube#video")) {
-					// Thumbnail thumbnail =
-					// singleVideo.getSnippet().getThumbnails().getDefault();
-					// System.out.println(singleVideo);
-					System.out.println("https://www.youtube.com/watch?v=" + rId.getVideoId());
-					System.out.println(singleVideo.getSnippet().getTitle());
-
-					try {
-
-						writer3.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
-						writer3.write(singleVideo.getSnippet().getTitle());
-						writer3.write(System.lineSeparator());
-						counter++;
-						System.out.println(counter);
-
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			}
-
-		}
-
-		else if (threadNumber == 4) {
-			List<SearchResult> searchResults = search.getVideoLinkFromKeyWord(keyword);
-			ListIterator<SearchResult> iteratorSearchResults = searchResults.listIterator();
-			System.out.println("\n=============================================================");
-			System.out.println(
-					"   First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + keyword + "\".");
-			System.out.println("=============================================================\n");
-
-			if (!iteratorSearchResults.hasNext()) {
-				System.out.println(" There aren't any results for your query.");
-			}
-
-			while (iteratorSearchResults.hasNext()) {
-
-				SearchResult singleVideo = iteratorSearchResults.next();
-				ResourceId rId = singleVideo.getId();
-
-				// Confirm that the result represents a video. Otherwise, the
-				// item will not contain a video ID.
-				if (rId.getKind().equals("youtube#video")) {
-					// Thumbnail thumbnail =
-					// singleVideo.getSnippet().getThumbnails().getDefault();
-					// System.out.println(singleVideo);
-					System.out.println("https://www.youtube.com/watch?v=" + rId.getVideoId());
-					System.out.println(singleVideo.getSnippet().getTitle());
-
-					try {
-
-						writer4.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
-						writer4.write(singleVideo.getSnippet().getTitle());
-						writer4.write(System.lineSeparator());
-						counter++;
-						System.out.println(counter);
-
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			}
-
-		}
+		// else if (threadNumber == 2) {
+		// List<SearchResult> searchResults =
+		// search.getVideoLinkFromKeyWord(keyword);
+		// ListIterator<SearchResult> iteratorSearchResults =
+		// searchResults.listIterator();
+		// System.out.println("\n=============================================================");
+		// System.out.println(
+		// " First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on
+		// \"" + keyword + "\".");
+		// System.out.println("=============================================================\n");
+		//
+		// if (!iteratorSearchResults.hasNext()) {
+		// System.out.println(" There aren't any results for your query.");
+		// }
+		//
+		// while (iteratorSearchResults.hasNext()) {
+		//
+		// SearchResult singleVideo = iteratorSearchResults.next();
+		// ResourceId rId = singleVideo.getId();
+		//
+		// // Confirm that the result represents a video. Otherwise, the
+		// // item will not contain a video ID.
+		// if (rId.getKind().equals("youtube#video")) {
+		// // Thumbnail thumbnail =
+		// // singleVideo.getSnippet().getThumbnails().getDefault();
+		// // System.out.println(singleVideo);
+		// System.out.println("https://www.youtube.com/watch?v=" +
+		// rId.getVideoId());
+		// System.out.println(singleVideo.getSnippet().getTitle());
+		//
+		// try {
+		//
+		// writer2.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
+		// writer2.write(singleVideo.getSnippet().getTitle());
+		// writer2.write(System.lineSeparator());
+		// counter++;
+		// System.out.println(counter);
+		//
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+		// }
+		// }
+		// } else if (threadNumber == 3) {
+		// List<SearchResult> searchResults =
+		// search.getVideoLinkFromKeyWord(keyword);
+		// ListIterator<SearchResult> iteratorSearchResults =
+		// searchResults.listIterator();
+		// System.out.println("\n=============================================================");
+		// System.out.println(
+		// " First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on
+		// \"" + keyword + "\".");
+		// System.out.println("=============================================================\n");
+		//
+		// if (!iteratorSearchResults.hasNext()) {
+		// System.out.println(" There aren't any results for your query.");
+		// }
+		//
+		// while (iteratorSearchResults.hasNext()) {
+		//
+		// SearchResult singleVideo = iteratorSearchResults.next();
+		// ResourceId rId = singleVideo.getId();
+		//
+		// // Confirm that the result represents a video. Otherwise, the
+		// // item will not contain a video ID.
+		// if (rId.getKind().equals("youtube#video")) {
+		// // Thumbnail thumbnail =
+		// // singleVideo.getSnippet().getThumbnails().getDefault();
+		// // System.out.println(singleVideo);
+		// System.out.println("https://www.youtube.com/watch?v=" +
+		// rId.getVideoId());
+		// System.out.println(singleVideo.getSnippet().getTitle());
+		//
+		// try {
+		//
+		// writer3.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
+		// writer3.write(singleVideo.getSnippet().getTitle());
+		// writer3.write(System.lineSeparator());
+		// counter++;
+		// System.out.println(counter);
+		//
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+		// }
+		// }
+		//
+		// }
+		//
+		// else if (threadNumber == 4) {
+		// List<SearchResult> searchResults =
+		// search.getVideoLinkFromKeyWord(keyword);
+		// ListIterator<SearchResult> iteratorSearchResults =
+		// searchResults.listIterator();
+		// System.out.println("\n=============================================================");
+		// System.out.println(
+		// " First " + Search.NUMBER_OF_VIDEOS_RETURNED + " videos for search on
+		// \"" + keyword + "\".");
+		// System.out.println("=============================================================\n");
+		//
+		// if (!iteratorSearchResults.hasNext()) {
+		// System.out.println(" There aren't any results for your query.");
+		// }
+		//
+		// while (iteratorSearchResults.hasNext()) {
+		//
+		// SearchResult singleVideo = iteratorSearchResults.next();
+		// ResourceId rId = singleVideo.getId();
+		//
+		// // Confirm that the result represents a video. Otherwise, the
+		// // item will not contain a video ID.
+		// if (rId.getKind().equals("youtube#video")) {
+		// // Thumbnail thumbnail =
+		// // singleVideo.getSnippet().getThumbnails().getDefault();
+		// // System.out.println(singleVideo);
+		// System.out.println("https://www.youtube.com/watch?v=" +
+		// rId.getVideoId());
+		// System.out.println(singleVideo.getSnippet().getTitle());
+		//
+		// try {
+		//
+		// writer4.write("https://www.youtube.com/watch?v=" + rId.getVideoId());
+		// writer4.write(singleVideo.getSnippet().getTitle());
+		// writer4.write(System.lineSeparator());
+		// counter++;
+		// System.out.println(counter);
+		//
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+		// }
+		// }
+		//
+		// }
 
 	}
 
--- src/main/java/no/uio/ifi/models/Search.java
+++ src/main/java/no/uio/ifi/models/Search.java
@@ -25,6 +25,8 @@ import no.uio.ifi.management.ManagementAllRandom;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoListResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,84 +41,136 @@ import java.util.Properties;
 public class Search {
 	ManagementAll mng;
 	ManagementAllRandom mng2;
-    /**
-     * Define a global variable that identifies the name of a file that
-     * contains the developer's API key.
-     */
-    public static final String PROPERTIES_FILENAME = "youtube.properties";
+	/**
+	 * Define a global variable that identifies the name of a file that contains
+	 * the developer's API key.
+	 */
+	public static final String PROPERTIES_FILENAME = "youtube.properties";
 
-    public static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+	public static final long NUMBER_OF_VIDEOS_RETURNED = 25;
 
-    /**
-     * Define a global instance of a Youtube object, which will be used
-     * to make YouTube Data API requests.
-     */
-    private static YouTube youtube;
-    
-    public Search(ManagementAll mng){
-    	this.mng = mng;
-    }
-    
-    public Search(ManagementAllRandom mng){
-    	this.mng2 = mng;
-    }
-    
-    public List<SearchResult> getVideoLinkFromKeyWord(String queryTerm){
-    	// Read the developer key from the properties file.
-    	Properties properties = new Properties();
-    	List<SearchResult> searchResultList = null;
-        try {
-            InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
-            properties.load(in);
+	/**
+	 * Define a global instance of a Youtube object, which will be used to make
+	 * YouTube Data API requests.
+	 */
+	private static YouTube youtube;
 
-        } catch (IOException e) {
-            System.err.println("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause()
-                    + " : " + e.getMessage());
-            System.exit(1);
-        }
+	public Search(ManagementAll mng) {
+		this.mng = mng;
+	}
 
-        try {
-            // This object is used to make YouTube Data API requests. The last
-            // argument is required, but since we don't need anything
-            // initialized when the HttpRequest is initialized, we override
-            // the interface and provide a no-op function.
-            youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
-                public void initialize(HttpRequest request) throws IOException {
-                }
-            }).setApplicationName("youtube-cmdline-search-sample").build();
-            
+	public Search(ManagementAllRandom mng) {
+		this.mng2 = mng;
+	}
 
-            // Define the API request for retrieving search results.
-            YouTube.Search.List search = youtube.search().list("id,snippet");
+	public List<SearchResult> getVideoLinkFromKeyWord(String queryTerm) {
+		// Read the developer key from the properties file.
+		Properties properties = new Properties();
+		List<SearchResult> searchResultList = null;
+		try {
+			InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
+			properties.load(in);
 
-            // Set your developer key from the {{ Google Cloud Console }} for
-            // non-authenticated requests. See:
-            // {{ https://cloud.google.com/console }}
-            String apiKey = properties.getProperty("youtube.apikey");
-            search.setKey(apiKey);
-            search.setQ(queryTerm);
+		} catch (IOException e) {
+			System.err.println(
+					"There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
+			System.exit(1);
+		}
 
-            // Restrict the search results to only include videos. See:
-            // https://developers.google.com/youtube/v3/docs/search/list#type
-            search.setType("video");
+		try {
+			// This object is used to make YouTube Data API requests. The last
+			// argument is required, but since we don't need anything
+			// initialized when the HttpRequest is initialized, we override
+			// the interface and provide a no-op function.
+			youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+				public void initialize(HttpRequest request) throws IOException {
+				}
+			}).setApplicationName("youtube-cmdline-search-sample").build();
 
-            // To increase efficiency, only retrieve the fields that the
-            // application uses.
-            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-            search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+			// Define the API request for retrieving search results.
+			YouTube.Search.List search = youtube.search().list("id,snippet");
 
-            // Call the API and print results.
-            SearchListResponse searchResponse = search.execute();
-            searchResultList = searchResponse.getItems();
-           
-        } catch (GoogleJsonResponseException e) {
-            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
-                    + e.getDetails().getMessage());
-        } catch (IOException e) {
-            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-    	return searchResultList;
-    }
+			// Set your developer key from the {{ Google Cloud Console }} for
+			// non-authenticated requests. See:
+			// {{ https://cloud.google.com/console }}
+			String apiKey = properties.getProperty("youtube.apikey");
+			search.setKey(apiKey);
+			search.setQ(queryTerm);
+
+			// Restrict the search results to only include videos. See:
+			// https://developers.google.com/youtube/v3/docs/search/list#type
+			search.setType("video");
+
+			// To increase efficiency, only retrieve the fields that the
+			// application uses.
+			search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+			search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+
+			// Call the API and print results.
+			SearchListResponse searchResponse = search.execute();
+			searchResultList = searchResponse.getItems();
+
+		} catch (GoogleJsonResponseException e) {
+			System.err.println(
+					"There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+		} catch (IOException e) {
+			System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		return searchResultList;
+	}
+
+	public List<Video> getVideoList(String queryTerm) {
+
+		List<Video> videoList = null;
+		Properties properties = new Properties();
+		try {
+			InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
+			properties.load(in);
+
+		} catch (IOException e) {
+			System.err.println(
+					"There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
+			System.exit(1);
+		}
+
+		try {
+			// This object is used to make YouTube Data API requests. The last
+			// argument is required, but since we don't need anything
+			// initialized when the HttpRequest is initialized, we override
+			// the interface and provide a no-op function.
+			youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+				@Override
+				public void initialize(HttpRequest request) throws IOException {
+				}
+			}).setApplicationName("youtube-cmdline-geolocationsearch-sample").build();
+
+			// Set your developer key from the Google Developers Console for
+			// non-authenticated requests. See:
+			// https://console.developers.google.com/
+			String apiKey = properties.getProperty("youtube.apikey");
+
+			// Call the YouTube Data API's youtube.videos.list method to
+			// retrieve the resources that represent the specified videos.
+			YouTube.Videos.List listVideosRequest = youtube.videos()
+					.list("snippet, recordingDetails, contentDetails, statistics").setId(queryTerm);
+
+			// Set your developer key
+			listVideosRequest.setKey(apiKey);
+
+			VideoListResponse listResponse = listVideosRequest.execute();
+			videoList = listResponse.getItems();
+
+		} catch (GoogleJsonResponseException e) {
+			System.err.println(
+					"There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+		} catch (IOException e) {
+			System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+		return videoList;
+	}
 }
